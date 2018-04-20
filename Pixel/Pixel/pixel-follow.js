847a22a800f486e435f46701a1fb3bb31e423f8f
Dom Barter <dominicbarter@gmail.com>
2018-04-20 20:29:51 +0100

    successfully created object

---
+++ Pixel/Pixel/pixel-follow.js
@@ -0,0 +1,35 @@
+﻿function createCanvas(width, height, backgroundColor,canvasId) {
+    var canvasCreate = document.createElement('canvas');
+    canvasCreate.id = canvasId;
+    canvasCreate.width = width;
+    canvasCreate.height = height;
+    canvasCreate.style.backgroundColor = backgroundColor;
+    canvasCreate.style.top = "0px";
+    canvasCreate.style.left = "0px";
+    canvasCreate.style.position = "absolute";
+    document.body.appendChild(canvasCreate);
+}
+
+function square(sideLength, color, startX, startY) {
+    this.x = startX;
+    this.y = startY;
+    this.sideLength = sideLength;
+    this.color = color;
+}
+
+createCanvas(window.innerWidth, window.innerHeight, "#171717", "pixelCanvas"); //create canvas
+
+var canvasId = document.getElementById("pixelCanvas");
+var canvas = canvasId.getContext("2d");
+
+var squareOne = new square(25, "#FF6C00",0,0);
+
+canvas.fillStyle = squareOne.color;
+canvas.fillRect(squareOne.x, squareOne.y, squareOne.sideLength, squareOne.sideLength);
+
+
+canvasId.addEventListener("click", function () {
+    alert("hello");
+})
+
+
