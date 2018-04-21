0dfab95dc787af21e79708507aec9b05b6fce8db
Dom Barter <dominicbarter@gmail.com>
2018-04-21 11:27:06 +0100

    commit

--- Pixel/Pixel/pixel.js
+++ Pixel/Pixel/pixel.js
@@ -20,7 +20,8 @@ function pixel(sideLength, color, startX, startY) {
     this.mass = 1;
     this.bounce = 1;
     this.gravity = 10;
-    this.speed = 10;
+    this.xVelocity = 10;
+    this.yVelocity = 10;
 }
 
 //where all pixels are stored
@@ -31,6 +32,10 @@ createCanvas(window.innerWidth, window.innerHeight, "#171717", "pixelCanvas");
 //canvas config
 var canvasId = document.getElementById("pixelCanvas");
 var canvas = canvasId.getContext("2d");
+
+//width & height
+var width = window.innerWidth;
+var height = window.innerHeight;
 
 //redraw function
 function redraw() {
@@ -45,8 +50,34 @@ function redraw() {
 //move function
 function move() {
     for (var a = 0; a < arrayOfPixels.length; a++) {
-        arrayOfPixels[a].x += arrayOfPixels[a].speed;
-        arrayOfPixels[a].y += arrayOfPixels[a].speed;
+
+        //hitting x max border
+        if (arrayOfPixels[a].x > width - arrayOfPixels[a].sideLength) {
+            arrayOfPixels[a].xVelocity = -1 * (arrayOfPixels[a].xVelocity);
+            arrayOfPixels[a].xVelocity = (arrayOfPixels[a].xVelocity) * (arrayOfPixels[a].bounce);
+        }
+
+        //hitting y  max border
+        if (arrayOfPixels[a].y > height - arrayOfPixels[a].sideLength) {
+            arrayOfPixels[a].yVelocity = -1 * (arrayOfPixels[a].yVelocity);
+            arrayOfPixels[a].yVelocity = (arrayOfPixels[a].yVelocity) * (arrayOfPixels[a].bounce);
+        }
+
+        //hitting x min border
+        if (arrayOfPixels[a].x < 0) {
+            arrayOfPixels[a].xVelocity = -1 * (arrayOfPixels[a].xVelocity);
+            arrayOfPixels[a].xVelocity = (arrayOfPixels[a].xVelocity) * (arrayOfPixels[a].bounce);
+        }
+
+        //hitting y min border
+        if (arrayOfPixels[a].y < 0) {
+            arrayOfPixels[a].yVelocity = -1 * (arrayOfPixels[a].yVelocity);
+            arrayOfPixels[a].yVelocity = (arrayOfPixels[a].yVelocity) * (arrayOfPixels[a].bounce);
+        }
+
+        //changing x & y locations
+        arrayOfPixels[a].x += arrayOfPixels[a].xVelocity;
+        arrayOfPixels[a].y += arrayOfPixels[a].yVelocity;
     }
 }
 
@@ -56,7 +87,7 @@ arrayOfPixels.push(pixelOne);
 
 //creating second pixel
 var pixelTwo = new pixel(20, "#FF6C00", 0, 0)
-pixelTwo.speed = 1;
+pixelTwo.bounce = 0.5;
 arrayOfPixels.push(pixelTwo);
 
 //starting redraw and move functions
